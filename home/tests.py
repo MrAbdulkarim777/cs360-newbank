08a3e907f2df77a7c9b47b7ddf7d1aa6714462dd
Abdulkarim Shavkatov <a.shavkatov@newuu.uz>
2026-07-03 07:00:55 -0400

    Adding failing test

--- home/tests.py
+++ home/tests.py
@@ -8,19 +8,18 @@ class CardDetailTests(TestCase):
         valid_cards = ['Visa', 'MasterCard', 'Humo', 'Uzcard']
         for card in valid_cards:
             # urls.py dagi name='card_detail' ga argument berib chaqiramiz
-            url = reverse('home:card_detail', kwargs={'card_type': card})
+            url = reverse('home:card_detail', args=[card])
             response = self.client.get(url)
             self.assertEqual(response.status_code, 200)
 
     def test_invalid_card_type_returns_404(self):
         """Tizimda yo'q karta turi (masalan, Azizbek) kiritilsa 404 qaytarishi kerak"""
-        url = reverse('home:card_detail', kwargs={'card_type': 'Azizbek'})
+        url = reverse('home:card_detail', args=['Azizbek'])
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
 class CurrencyViewTDDTests(TestCase):
 
     def test_currency_view_contains_uzs(self):
         response = self.client.get('/currency/')
-
         self.assertEqual(response.status_code, 200)
         self.assertIn('UZS', response.context['currency_data'])
